8fd7571a674379e13ae5b132cab3e3e63a8ca6b2
Stephen McDonald <steve@jupo.org>
2012-08-19 01:18:43 +1000

    docstring for GameForm

--- core/forms.py
+++ core/forms.py
@@ -7,6 +7,9 @@ class GameForm(forms.Form):
     amount = forms.IntegerField(initial=100)
 
     def __init__(self, *args, **kwargs):
+        """
+        Add some HTML5 attributes to form fields.
+        """
         super(GameForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             if isinstance(field, forms.IntegerField):
