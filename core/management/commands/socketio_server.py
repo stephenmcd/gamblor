518bba784f2b4efe1f57ec6f43aa2b74f2fb1e53
Stephen McDonald <steve@jupo.org>
2012-08-19 01:29:51 +1000

    Run game.autodiscover for socketio server.

--- core/management/commands/socketio_server.py
+++ core/management/commands/socketio_server.py
@@ -3,11 +3,13 @@ from django.core.management.base import BaseCommand
 from socketio.server import SocketIOServer
 
 from core.app import Application
+from core import game
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        game.autodiscover()
         SocketIOServer(("0.0.0.0", 9000), Application()).serve_forever()
 
 
