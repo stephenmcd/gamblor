21c35e176f275f914b505079fa7f964b5b1fb791
Stephen McDonald <steve@jupo.org>
2012-08-19 00:08:53 +1000

    Move game specific JS into its own script.

--- static/js/gamblor.js
+++ static/js/gamblor.js
@@ -31,10 +31,6 @@ $(function() {
         $('.game-' + game + ' .players').html('');
     });
 
-    socket.on('roulette_landed_on', function(landed_on) {
-        alert('Roulette landed on: ' + landed_on);
-    });
-
     socket.emit('start');
 
     $('.game form').each(function(i, form) {
