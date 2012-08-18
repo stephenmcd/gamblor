02879ea078f0c465c608ff6920720ede076d83bb
Stephen McDonald <steve@jupo.org>
2012-08-18 13:38:21 +1000

    Initial redis integration / user auth and comms

--- core/app.py
+++ core/app.py
@@ -1,10 +1,40 @@
 
+from Cookie import Cookie
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
 from socketio import socketio_manage
+from socketio.mixins import BroadcastMixin
 from socketio.namespace import BaseNamespace
+from redis import Redis, ConnectionPool
 
 
-class GamblorNamespace(BaseNamespace):
-    pass
+redis = Redis(connection_pool=ConnectionPool())
+
+
+class GamblorNamespace(BaseNamespace, BroadcastMixin):
+
+    def on_start(self):
+        try:
+            cookie = Cookie(self.environ["HTTP_COOKIE"])
+            session_key = cookie["sessionid"].value
+            session = Session.objects.get(session_key=session_key)
+            user_id = session.get_decoded().get("_auth_user_id")
+            user = User.objects.get(id=user_id)
+        except (KeyError, ObjectDoesNotExist):
+            self.user = None
+        else:
+            self.user = {"name": user.username, "id": user.id}
+            self.broadcast_event_not_me("join", self.user)
+            redis.sadd("users", self.user)
+        self.emit("users", list(redis.smembers("users")))
+
+    def recv_disconnect(self):
+        self.disconnect()
+        if self.user:
+            redis.srem("users", self.user)
+            self.broadcast_event_not_me("leave", self.user)
 
 
 class Application(object):
--- static/js/gamblor.js
+++ static/js/gamblor.js
@@ -1,1 +1,23 @@
-// gamblor js
+
+$(function() {
+
+    var socket = io.connect(':9000', {
+        transports: ['websocket', 'htmlfile', 'xhr-multipart',
+                     'xhr-polling', 'jsonp-polling']
+    });
+
+    socket.on('users', function(users) {
+        console.log('users: ', users);
+    });
+
+    socket.on('join', function(user) {
+        console.log('joined: ', user);
+    });
+
+    socket.on('leave', function(user) {
+        console.log('left: ', user);
+    });
+
+    socket.emit('start');
+
+});
