ed050f3c9181502990a52eccc008f4bb2f7ba441
Stephen McDonald <steve@jupo.org>
2012-08-18 23:28:49 +1000

    Add some html5 attribs to bet forms.

--- core/forms.py
+++ core/forms.py
@@ -5,3 +5,12 @@ from django import forms
 class GameForm(forms.Form):
 
     amount = forms.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super(GameForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            if isinstance(field, forms.IntegerField):
+                self.fields[name].widget.input_type = "number"
+            if field.required:
+                self.fields[name].widget.attrs["required"] = ""
+
