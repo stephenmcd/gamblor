20cdf9a028de88a422bf7b2c0fe0f66e53432908
Stephen McDonald <steve@jupo.org>
2012-08-18 23:07:16 +1000

    No game delay with DEBUG on.

--- core/settings.py
+++ core/settings.py
@@ -6,4 +6,4 @@ from django.conf import settings
 DEFAULT_BALANCE = getattr(settings, "DEFAULT_BALANCE", 5000)
 
 # Number of seconds until a game is played, after the first player joins.
-BETTING_PERIOD = getattr(settings, "BETTING_PERIOD", 10)
+BETTING_PERIOD = getattr(settings, "BETTING_PERIOD", int(settings.DEBUG) * 10)
