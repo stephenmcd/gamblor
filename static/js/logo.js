76152370147a7b58bc953b26d555c5b85f6986f5
Stephen McDonald <steve@jupo.org>
2012-08-19 11:52:59 +1000

    Added faux neo-flickering JS

---
+++ static/js/logo.js
@@ -0,0 +1,23 @@
+
+$(function() {
+
+    var flicker = function() {
+        var logo = $('.logo');
+        var speed = 100;
+        var times = Math.ceil(3 * Math.random());
+        var once = function() {
+            logo.fadeOut(speed, function() {
+                logo.fadeIn(speed, function() {
+                    if (times-- > 0) {
+                        once();
+                    }
+                });
+            });
+        };
+        once();
+        setTimeout(flicker, 5000 + (10000 * Math.random()));
+    };
+
+    flicker();
+
+});
